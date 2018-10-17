e61940a55b9631e7792a4f687cfcdb9b23f14715
Allisson Azevedo <allisson@gmail.com>
2018-10-17 12:58:48 -0300

    Adicionando o restante dos endpoints

--- simple_rest_client_example/api.py
+++ simple_rest_client_example/api.py
@@ -5,11 +5,21 @@ from . import resources
 
 def get_api_instance(api_root_url='https://swapi.co/api/', timeout=5):
     api = API(api_root_url=api_root_url, timeout=timeout)
+    api.add_resource(resource_name='films', resource_class=resources.FilmsResource)
     api.add_resource(resource_name='people', resource_class=resources.PeopleResource)
+    api.add_resource(resource_name='planets', resource_class=resources.PlanetsResource)
+    api.add_resource(resource_name='species', resource_class=resources.SpeciesResource)
+    api.add_resource(resource_name='starships', resource_class=resources.StarshipsResource)
+    api.add_resource(resource_name='vehicles', resource_class=resources.VehiclesResource)
     return api
 
 
 def get_async_api_instance(api_root_url='https://swapi.co/api/', timeout=5):
     api = API(api_root_url=api_root_url, timeout=timeout)
+    api.add_resource(resource_name='films', resource_class=resources.FilmsAsyncResource)
     api.add_resource(resource_name='people', resource_class=resources.PeopleAsyncResource)
+    api.add_resource(resource_name='planets', resource_class=resources.PlanetsAsyncResource)
+    api.add_resource(resource_name='species', resource_class=resources.SpeciesAsyncResource)
+    api.add_resource(resource_name='starships', resource_class=resources.StarshipsAsyncResource)
+    api.add_resource(resource_name='vehicles', resource_class=resources.VehiclesAsyncResource)
     return api
--- simple_rest_client_example/resources.py
+++ simple_rest_client_example/resources.py
@@ -1,26 +1,140 @@
 from simple_rest_client.resource import AsyncResource, Resource
 
 
-def build_actions(resource_name):
-    return {
-        'list': {
-            'method': 'GET',
-            'url': resource_name
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': resource_name + '/{}',
-        },
-        'schema': {
-            'method': 'GET',
-            'url': resource_name + '/{}',
-        },
-    }
+films_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'films'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'films/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'films/schema',
+    },
+}
+
+people_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'people'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'people/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'people/schema',
+    },
+}
+
+planets_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'planets'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'planets/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'planets/schema',
+    },
+}
+
+species_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'species'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'species/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'species/schema',
+    },
+}
+
+starships_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'starships'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'starships/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'starships/schema',
+    },
+}
+
+vehicles_actions = {
+    'list': {
+        'method': 'GET',
+        'url': 'vehicles'
+    },
+    'retrieve': {
+        'method': 'GET',
+        'url': 'vehicles/{}',
+    },
+    'schema': {
+        'method': 'GET',
+        'url': 'vehicles/schema',
+    },
+}
+
+
+class FilmsAsyncResource(AsyncResource):
+    actions = films_actions
+
+
+class FilmsResource(Resource):
+    actions = films_actions
 
 
 class PeopleAsyncResource(AsyncResource):
-    actions = build_actions('people')
+    actions = people_actions
 
 
 class PeopleResource(Resource):
-    actions = build_actions('people')
+    actions = people_actions
+
+
+class PlanetsAsyncResource(AsyncResource):
+    actions = planets_actions
+
+
+class PlanetsResource(Resource):
+    actions = planets_actions
+
+
+class SpeciesAsyncResource(AsyncResource):
+    actions = species_actions
+
+
+class SpeciesResource(Resource):
+    actions = species_actions
+
+
+class StarshipsAsyncResource(AsyncResource):
+    actions = starships_actions
+
+
+class StarshipsResource(Resource):
+    actions = starships_actions
+
+
+class VehiclesAsyncResource(AsyncResource):
+    actions = vehicles_actions
+
+
+class VehiclesResource(Resource):
+    actions = vehicles_actions
